3521751cdd3ae9d3b718ef6ca72c528d3f4f4e39
Anik Ahmed <anikahmed0091@gmail.com>
2022-04-30 15:36:45 +0600

    🐞 fix: theme change

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,11 @@ import Layout from "../components/UI/Layout";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Layout>
-        <ThemeProvider enableSystem={true} attribute="class">
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Layout>
           <Component {...pageProps} />
-        </ThemeProvider>
-      </Layout>
+        </Layout>
+      </ThemeProvider>
     </>
   );
 }
