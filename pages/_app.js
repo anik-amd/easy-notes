4ab0cd6d126660f9e26c99ea7746c59f759d6f62
Anik Ahmed <anikahmed0091@gmail.com>
2022-04-30 15:30:25 +0600

    🐞 fix: layout placement

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import Layout from "../components/UI/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <ThemeProvider enableSystem={true} attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Layout>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Layout>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-import Layout from "../components/UI/Layout";
 import Notes from "../components/partials/Notes";
 import New from "../components/partials/New";
 
 const Home = () => {
   return (
-    <Layout>
+    <>
       <New />
       <Notes />
-    </Layout>
+    </>
   );
 };
 
