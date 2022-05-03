39e5d899b3c3c832acac33fb57c60a38203d90c9
Anik Ahmed <anikahmed0091@gmail.com>
2022-05-03 20:52:31 +0600

    ✨ feat: add persistant state

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,21 @@ import Layout from "../components/UI/Layout";
 import store from "../store/store";
 import { Provider } from "react-redux";
 
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
 function MyApp({ Component, pageProps }) {
+  let persistor = persistStore(store);
   return (
     <>
       <Provider store={store}>
-        <ThemeProvider enableSystem={true} attribute="class">
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ThemeProvider>
+        <PersistGate persistor={persistor}>
+          <ThemeProvider enableSystem={true} attribute="class">
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </ThemeProvider>
+        </PersistGate>
       </Provider>
     </>
   );
--- store/store.js
+++ store/store.js
@@ -1,8 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
-import notesReducer from "./notesSlice";
+// import notesReducer from "./notesSlice";
+
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import notesSlice from "./notesSlice";
+
+const persistConfig = {
+  key: "notes",
+  storage,
+};
+
+const reducers = combineReducers({ notes: notesSlice });
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+// export default configureStore({
+//   reducer: {
+//     notes: notesReducer,
+//   },
+// });
 
 export default configureStore({
-  reducer: {
-    notes: notesReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
